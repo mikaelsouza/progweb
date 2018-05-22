71a4f7d446eba9b621900ac2787e8e370936bbd2
Mikael Souza <mss2@icomp.ufam.edu.br>
2018-05-22 18:49:24 -0400

    Adicionadas regras 1, 2 e 3

--- skifree/js/skifree.js
+++ skifree/js/skifree.js
@@ -1,6 +1,6 @@
 (function () {
 
-    const FPS = 50;
+    const FPS = 60;
     const TAMX = 300;
     const TAMY = 400;
     const PROB_ARVORE = 2;
@@ -9,11 +9,15 @@
     var skier;
     var direcoes = ['para-esquerda', 'para-frente', 'para-direita']
     var arvores = [];
+    var vidas_div;
+    var distancia_div;
 
     function init() {
         montanha = new Montanha();
         skier = new Skier();
         arvores = [];
+        vidas_div = document.getElementById("vidas");
+        distancia_div = document.getElementById("distancia");
         gameLoop = setInterval(run, 1000 / FPS);
     }
 
@@ -24,14 +28,19 @@
             a.element.remove();
         });
         arvores = [];
+        vidas_div.innerHTML = "";
+        distancia_div.innerHTML = "";
 
     }
 
     window.addEventListener('keydown', function (e) {
         if (e.key == 'a') skier.mudarDirecao(-1);
         else if (e.key == 'd') skier.mudarDirecao(1);
-        else if (e.key == 's') skier.acelerar();
-        else if (e.key == 'w') skier.frear();
+        else if (e.key == 'f') skier.acelerar();
+    });
+
+    window.addEventListener('keyup', function (e) {
+        if (e.key == 'f') skier.frear();
     });
 
     function Montanha() {
@@ -46,11 +55,13 @@
         this.direcao = 1; //0-esquerda;1-frente;2-direita
         this.x_vel = 1;
         this.y_vel = 1;
+        this.acelerado = false;
         this.vidas = 3;
         this.invuneravel = false;
         this.element.className = 'para-frente';
         this.element.style.top = '30px';
         this.element.style.left = parseInt(TAMX / 2) - 7 + 'px';
+        this.distancia = 0;
 
         this.mudarDirecao = function (giro) {
             if (this.direcao + giro >= 0 && this.direcao + giro <= 2) {
@@ -59,23 +70,44 @@
             }
         }
 
+        this.setarDirecao = function (dir) {
+            this.direcao = dir;
+            this.element.className = direcoes[this.direcao];
+        }
+
         this.andar = function () {
+            var x0_position = parseInt(this.element.style.left);
+            var x1_position = parseInt(this.element.clientWidth) + x0_position;
+            if (x0_position < 0) {
+                this.setarDirecao(1);
+                this.element.style.left = x0_position + 1 + "px";
+            } else if (x1_position >= TAMX) {
+                this.setarDirecao(1);
+                this.element.style.left = parseInt(this.element.style.left) - 2 + "px";
+            }
+
             if (this.direcao == 0) {
                 this.element.style.left = (parseInt(this.element.style.left) - skier.y_vel) + "px";
-            }
-            if (this.direcao == 2) {
+            } else if (this.direcao == 2) {
                 this.element.style.left = (parseInt(this.element.style.left) + skier.y_vel) + "px";
+            }
+            if (this.acelerado) {
+                this.distancia += 30 / FPS;
+            } else {
+                this.distancia += 20 / FPS;
             }
         }
 
         this.acelerar = function () {
-            this.x_vel = 3;
+            this.x_vel = 2;
             this.y_vel = 2;
+            this.acelerado = true;
         }
 
         this.frear = function () {
             this.x_vel = 1;
             this.y_vel = 1;
+            this.acelerado = false;
         }
 
         this.dano = function () {
@@ -113,8 +145,6 @@
         return false;
     }
 
-
-
     function update() {
 
         // A cada frame calcula a chance de inserir novas árvores
@@ -126,18 +156,18 @@
         }
 
         // Remove árvores que estão fora da tela
-
         for (var i = arvores.length - 1; i >= 0; i--) {
             var arvore_y = parseInt(arvores[i].element.style.top);
             if (arvore_y < -40) {
                 arvores[i].element.remove();
                 arvores.splice(i, 1);
+                break;
             }
             if (arvores[i].verificaColisao()) {
                 skier.dano();
+                break;
             }
         }
-
         skier.andar();
     }
 
@@ -146,6 +176,8 @@
             var arvore_y = parseInt(arvores[i].element.style.top);
             arvores[i].element.style.top = (arvore_y - skier.x_vel) + "px";
         }
+        distancia_div.innerHTML = Math.round(skier.distancia);
+        vidas_div.innerHTML = skier.vidas;
     }
 
     function run() {
