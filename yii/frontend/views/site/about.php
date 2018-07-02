e0b0fed90ff7246ba8d1640325769d275c46714d
Mikael Souza <mss2@icomp.ufam.edu.br>
2018-07-02 13:08:05 -0400

    Adicionada pasta Yii e primeiro exercício de Yii

---
+++ yii/frontend/views/site/about.php
@@ -0,0 +1,18 @@
+<?php
+
+/* @var $this yii\web\View */
+
+use yii\helpers\Html;
+
+$this->title = 'About';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="site-about">
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>Este é o website do jogo Skifree implementado por Mikael Souza como atividade de Programação Web.</p>
+
+    <p>Data: <?= $date ?></p>
+
+    <code><?= __FILE__ ?></code>
+</div>
