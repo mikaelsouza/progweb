8401d8d197b47ff7a98cbbb848fc25b67b80383a
Mikael Souza <mss2@icomp.ufam.edu.br>
2018-07-03 14:59:55 -0400

    Atualizado nome de id para nome

--- yii/frontend/views/curso/update.php
+++ yii/frontend/views/curso/update.php
@@ -5,7 +5,7 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 /* @var $model common\models\Curso */
 
-$this->title = 'Update Curso: ' . $model->id;
+$this->title = 'Update Curso: ' . $model->nome;
 $this->params['breadcrumbs'][] = ['label' => 'Cursos', 'url' => ['index']];
 $this->params['breadcrumbs'][] = ['label' => $model->id, 'url' => ['view', 'id' => $model->id]];
 $this->params['breadcrumbs'][] = 'Update';
